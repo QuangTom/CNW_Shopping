6cb2b84a1cb641c1f102a538a8692b6994343e26
TrangHuyen <tmr.huyentrang@gmail.com>
2019-05-16 10:14:40 +0700

    ThemDanhMuc(Form)

--- CNW_Shopping/CNW_Shopping/Areas/Admin/Controllers/DanhMucController.cs
+++ CNW_Shopping/CNW_Shopping/Areas/Admin/Controllers/DanhMucController.cs
@@ -19,5 +19,27 @@ namespace CNW_Shopping.Areas.Admin.Controllers
             return View(model);
  
         }
+        // GET: Admin/Demo/Create
+        public ActionResult ThemDanhMuc()
+        {
+            return View();
+        }
+
+        // POST: Admin/Demo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
